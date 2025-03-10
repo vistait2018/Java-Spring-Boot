b3e9c761f9d1ea9aedc0e7aa2a381134970678c9
Java20091@ <jidedorcas@gmail.com>
2025-03-10 13:24:08 +0100

    Logging

--- src/main/java/com/pks/example/component/HelloWorldComponent.java
+++ src/main/java/com/pks/example/component/HelloWorldComponent.java
@@ -1,11 +1,20 @@
 package com.pks.example.component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloWorldComponent {
 
-    public String sayHello(){
-    return "Hello from COmponent";
+  Logger log =  LoggerFactory.getLogger(HelloWorldComponent.class);
+
+  public String sayHello(){
+
+     log.info("Info message");
+     log.error("Error Logs");
+      log.trace("Error Logs");
+      log.debug("Error Logs");
+    return "Hello from Component";
     }
 }
